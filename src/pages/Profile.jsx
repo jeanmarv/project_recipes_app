bf38bdd42c120c3500ff7700a19d09a78550b9d3
Polyana Balotin <polybalotin@hotmail.com>
2021-11-08 16:47:33 -0300

    header feito

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useHistory } from 'react-router';
+import Header from '../components/Header';
 
 export default function Profile() {
   const history = useHistory();
   const emailStorage = JSON.parse(localStorage.getItem('user'));
   return (
     <div>
+      <Header />
       <p data-testid="profile-email">{ emailStorage.email }</p>
       <button
         onClick={ () => history.push('/receitas-feitas') }
