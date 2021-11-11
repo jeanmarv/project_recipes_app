8e05b22e2a7d95c954c44b40a7d0938fad59c98d
Victor Martins <victor.martins.duarte@gmail.com>
2021-11-11 19:12:20 -0300

    Solving Lint errors

--- src/pages/ExploreFoodArea.jsx
+++ src/pages/ExploreFoodArea.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
-import React from 'react';
 import Header from '../components/Header';
 
 export default function ExploreFoodArea() {
