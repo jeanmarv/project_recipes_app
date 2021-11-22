99252149b519cb18a0e706b3f074743036b9bc9b
Polyana Balotin <polybalotin@hotmail.com>
2021-11-22 15:14:50 -0300

    requisitos 65 e 66

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
-export default function FavorireRecipes() {
+export default function FavoriteRecipe() {
+  const localStorageFavorite = JSON.parse(localStorage.getItem('favoriteRecipes')) || '';
+  const [favoriteRecipe, setFavoriteRecipe] = useState([]);
+  const [filterFavorites, setFilterFavorites] = useState('');
+
+  useEffect(() => {
+    setFavoriteRecipe(localStorageFavorite);
+  }, []);
+
+  function handleClick({ target }) {
+    setFilterFavorites(target.name);
+  }
+
   return (
-    <Header />
+    <>
+      <header className="header">
+        <Header />
+      </header>
+      <button
+        className="btn btn-outline-dark"
+        name=""
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ handleClick }
+      >
+        All
+      </button>
+      <button
+        className="btn btn-outline-dark"
+        name="comida"
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ handleClick }
+      >
+        Food
+      </button>
+      <button
+        className="btn btn-outline-dark"
+        name="bebida"
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ handleClick }
+      >
+        Drink
+      </button>
+      {localStorageFavorite.length > 0 && favoriteRecipe
+        .filter((recipe) => recipe.type === filterFavorites || filterFavorites === '')
+        .map((recipe, index) => (
+          <div key={ recipe.id }>
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <img
+                width="100px"
+                src={ recipe.image }
+                alt=""
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <h3
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${recipe.area || 'Alcoholic'} - ${recipe.category}`}
+              </h3>
+              <h2 data-testid={ `${index}-horizontal-name` }>
+                {recipe.name}
+              </h2>
+            </Link>
+
+          </div>
+        ))}
+    </>
   );
 }
