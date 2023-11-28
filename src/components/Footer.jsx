d03401a6a6cd465f39ddb2ad817e0849e71936f7
Jean <jeanvanzella91@gmail.com>
2023-11-28 15:45:11 -0300

    removed opacity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import drinkIcon from '../images/38706.png';
+import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import '../css/footer.css';
@@ -7,9 +7,9 @@ import '../css/footer.css';
 export default function Footer() {
   return (
     <footer className="footer" data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn" href="/bebidas">
+      <a data-testid="drinks-bottom-btn" href="/bebidas">
         <img src={ drinkIcon } alt="Icone de bebida" />
-      </button>
+      </a>
       <a data-testid="explore-bottom-btn" href="/explorar">
         <img src={ exploreIcon } alt="Icone de explorar" />
       </a>
